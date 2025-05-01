a7d9cf030e406baf28dedfe303822452f54feb75
wkambale <spartanwk@gmail.com>
2025-05-01 14:40:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="keras-gemini",
-    version="0.1.1",
+    version="0.1.2",
     description="A Python package that combines the power of Keras with Gemini for natural language-driven neural network building.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
